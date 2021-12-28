8200612647012495c525505dd79d5d4b01f3eeb3
Sora <sora.kim@vespainc.com>
2021-12-28 12:52:06 +0900

    [0107] Project settings

---
+++ AutoBM/ConsoleManager.cpp
@@ -0,0 +1,128 @@
+/*
+    Winter 2021
+    Automate your work assignment
+    Written by SeungGeon Kim
+
+    Filename : ConsoleManager.cpp
+	Edited : 2021-11-27
+*/
+
+#include "ConsoleManager.h"
+
+#include <windows.h>
+#include <iostream>
+
+
+
+ConsoleManager& ConsoleManager::Instance() noexcept
+{
+    static ConsoleManager instance;
+    return instance;
+}
+
+void ConsoleManager::SetupLocal() const noexcept
+{
+    std::locale mylocale("");
+    std::wcout.imbue(mylocale);
+}
+
+void ConsoleManager::PrintPosition(int row, int col, Color paramColor, bool paramIsNewLine) noexcept
+{
+    auto lambda = [=]()
+    {
+        std::wcout << "(" << row << ", " << col << ") -> ";
+    };
+
+    PrintHelper(lambda, paramColor, paramIsNewLine);
+}
+
+void ConsoleManager::PrintString(KR_STR paramText, Color paramColor, bool paramIsNewLine) noexcept
+{
+    auto lambda = [=]()
+    {
+        std::wcout << paramText;
+    };
+
+    PrintHelper(lambda, paramColor, paramIsNewLine);
+}
+
+void ConsoleManager::PrintString(std::string paramText, Color paramColor, bool paramIsNewLine) noexcept
+{
+    auto lambda = [=]()
+    {
+        std::wcout << paramText.c_str();
+    };
+
+    PrintHelper(lambda, paramColor, paramIsNewLine);
+}
+
+void ConsoleManager::PrintDouble(double paramDouble, Color paramColor, bool paramIsNewLine) noexcept
+{
+    auto lambda = [=]()
+    {
+        std::wcout << paramDouble;
+    };
+
+    PrintHelper(lambda, paramColor, paramIsNewLine);
+}
+
+void ConsoleManager::PrintDouble(int paramInt, Color paramColor, bool paramIsNewLine) noexcept
+{
+    auto lambda = [=]()
+    {
+        std::wcout << paramInt;
+    };
+
+    PrintHelper(lambda, paramColor, paramIsNewLine);
+}
+
+template <class T>
+void ConsoleManager::PrintHelper(T&& lambda, ConsoleManager::Color paramColor, bool paramIsNewLine) noexcept
+{
+    ConsoleManager::CheckTimer();
+
+    HANDLE hConsole;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, static_cast<WORD>(ConsoleManager::ReturnColor(paramColor)));
+
+    lambda();
+
+    if (paramIsNewLine == true)
+    {
+        std::wcout << std::endl;
+    }
+}
+
+int ConsoleManager::ReturnColor(Color paramColor) const noexcept
+{
+    switch (paramColor)
+    {
+    case Color::Green:
+        return 0xA;
+    case Color::Blue:
+        return 0x3;
+    case Color::Skyblue:
+        return 0xB;
+    case Color::Yellow:
+        return 0xE;
+    case Color::Red:
+        return 0xC;
+    case Color::Purple:
+        return 0xD;
+    }
+
+    return -1;
+}
+
+void ConsoleManager::CheckTimer() noexcept
+{
+    mClearTimer += 1;
+
+    if (mClearTimer >= REFRESH_RATE)
+    {
+        system("CLS");
+        mClearTimer = 0;
+    }
+}
---
+++ AutoBM/GlobalDefines.h
@@ -0,0 +1,28 @@
+/*
+    Winter 2021
+    Automate your work assignment
+    Written by SeungGeon Kim
+
+    Filename : GlobalDefines.h
+	Edited : 2021-11-27
+*/
+
+#pragma once
+
+#define KR_STR const wchar_t*
+
+#define REFRESH_RATE INT_MAX
+
+// ItemTable
+#define COL_ITEMTABLE_ITEMINDEX 1
+#define COL_ITEMTABLE_ITEMNAME 2
+#define COL_ITEMTABLE_ITEMDESC 3
+#define ROW_ITEMTABLE_FIRST 2
+
+// LocalTable
+#define COL_LOCALTABLE_ITEMINDEX 0
+#define COL_LOCALTABLE_TEXTINDEX 1
+#define ROW_LOCALTABLE_FIRST 1
+#define PREFIX_ITEMNAME L"ITEM_NAME_"
+#define PREFIX_ITEMDESC L"ITEM_DESC_"
+#define PREFIX_ITEMDESC2 L"ITEM_DESC2_"
