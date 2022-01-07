075806c25e9cb343eb5151e6d1bce904fdcb6fc0
Sora <sora.kim@vespainc.com>
2022-01-07 18:10:59 +0900

    wip

--- AutoBM/JsonDataWrapper.cpp
+++ AutoBM/JsonDataWrapper.cpp
@@ -198,7 +198,7 @@ void JsonDataWrapper::ConnectItemIcon() noexcept
         }
 
         PRINT_SAVING;
-        fileOutput << std::setw(4) << std::setfill(' ') << editorJson;
+        fileOutput << '\n' << std::setw(4) << std::setfill(' ') << editorJson;
         PRINT_SAVECOMPLETE;
 
         fileOutput.close();
